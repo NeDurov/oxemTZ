70742726561e8b336a5ac92f55135966bf3baa87
Max <maximabramov2000@yandex.ru>
2022-10-03 14:57:22 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./sass/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<form className="appForm">
+			<div className="appHeader">
+				Рассчитайте стоимость автомобиля в лизинг
+			</div>
+			<label className="carPrice">
+				<div className="title">Стоимость автомобиля</div>
+				<input type="text" className="input"></input>
+				<span className="lastSymbolCarPrice">&#8381;</span>
+
+				{/* RANGE */}
+			</label>
+
+			<label className="initialPayment">
+				<div className="title">Первоначальный взнос</div>
+				<input type="text" className="input"></input>
+				<div className="lastSymbolInitialPayment">13%</div>
+
+				{/* RANGE */}
+			</label>
+
+			<label className="leaseTerm">
+				<div className="title">Срок лизинга</div>
+				<input type="text" className="input"></input>\
+				<div className="lastSymbolLeaseTerm">мес.</div>
+				{/* RANGE */}
+			</label>
+			<div className="totalCost">
+				<div>Сумма договора лизинга</div>
+				<div className="total">
+					<h1>4 467 313</h1>
+					<h1 className="lastSymbolTotal"> &#8381;</h1>
+				</div>
+			</div>
+			<div className="monthlyPayment">
+				<div>Ежемесячный платеж от</div>
+				<div className="total">
+					<h1>114 455</h1>
+					<h1 className="lastSymbolTotal"> &#8381;</h1>
+				</div>
+			</div>
+			<input className="button" type="submit" value="Оставить заявку" />
+		</form>
+	);
 }
 
 export default App;
